124c3aa91974044d04ee9afed61c8701e5ec03ba
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-02-28 19:43:42 -0500

    trying for flip action

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('whats good fam')
+
+let cards = document.querySelectorAll('.card');
+console.log(cards);
+
+function flip() {
+  this.classList.toggle("flip")
+}
+
+for (let i = 0; i < cards.length; i ++) {
+  cards[i].addEventListener("click", flip())
+}
