3d076ab1a2180127ddc72fe004334816548137ad
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-01 12:22:25 -0500

    working on match logic and functions for dif click scenarios

--- script.js
+++ script.js
@@ -2,9 +2,44 @@ console.log('whats good fam')
 
 let cards = document.querySelectorAll('.card');
 console.log(cards);
+let flipped = false;
+let firstFlip;
+let secondFlip;
+
+function doesItMatch() {
+  if (firstFlip.dataset.language === secondFlip.dataset.language) {
+    stopListener();
+    return;
+  }
+  flipBack();
+}
+
+function stopListener() {
+  firstFlip.removeEventListener('click', flip);
+  secondFlip.removeEventListener('click', flip);
+}
+
+function flipBack() {
+  setTimeout(()=> {
+    firstFlip.classList.remove('flip');
+    secondFlip.classList.remove('flip');
+  }, 2000);
+}
 
 function flip() {
-  this.classList.toggle('flip')
+  this.classList.add('flip')
+
+  if (flipped !== true) {
+    flipped = true;
+    firstFlip = this;
+    return;
+  } else {
+    secondFlip = this;
+    flipped = false;
+
+    doesItMatch();
+  }
+
 }
 
 for (let i = 0; i < cards.length; i ++) {
@@ -12,3 +47,4 @@ for (let i = 0; i < cards.length; i ++) {
 }
 
 // cards.forEach(card => card.addEventListener('click', flip));
+
